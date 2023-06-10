228f1447194fbeaafb8fde6c48b03d0fede92441
Doğukan Gültürk <91672770+dogukangulturk@users.noreply.github.com>
2023-06-10 13:35:16 +0300

    Initial commit

---
+++ src/main/java/com/orcrist/springbootasyncexample/SpringbootAsyncExampleApplication.java
@@ -0,0 +1,13 @@
+package com.orcrist.springbootasyncexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAsyncExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAsyncExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orcrist/springbootasyncexample/SpringbootAsyncExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orcrist.springbootasyncexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAsyncExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
